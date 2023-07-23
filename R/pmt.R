ad4982991d10aa79448a25f6c21a85ba75df0b4f
qddyy <isduyan@outlook.com>
2023-07-24 00:55:43 +0800

    document pmts

--- R/pmt.R
+++ R/pmt.R
@@ -41,7 +41,8 @@ Tests <- as.environment(list(
 #' @export pmt
 pmt <- function(key, ...) do.call(key, list(...), envir = Tests)
 
-
+#' @title All tests available. 
+#' 
 #' @export pmts
 pmts <- data.frame(
     key = names(Tests),
