3497c30da893db33c9c205766ad2c3f2b47fe54b
qddyy <isduyan@outlook.com>
2024-11-11 21:24:48 +0800

    fix the documentation

--- man/roxygen/templates/pmt_init_params.R
+++ man/roxygen/templates/pmt_init_params.R
@@ -2,5 +2,5 @@
 #' @param scoring a character string specifying which scoring system to use.
 #' @param alternative a character string specifying the alternative hypothesis.
 #' @param conf_level a number between zero and one indicating the confidence level to use.
-#' @param n_permu an integer indicating number of permutations for the permutation distribution. If set to zero (default) then all permutations are used.
+#' @param n_permu an integer indicating number of permutations for the permutation distribution. If set to `0`, all permutations will be used.
 #' @param correct a logical indicating whether to apply continuity correction in the normal approximation for the p-value.
