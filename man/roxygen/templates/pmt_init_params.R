fd1721d22579529be9b07db6ca43fc5a2422ae03
qddyy <isduyan@outlook.com>
2024-11-12 14:21:30 +0800

    modified some text

--- man/roxygen/templates/pmt_init_params.R
+++ man/roxygen/templates/pmt_init_params.R
@@ -1,5 +1,5 @@
 #' @param type a character string specifying the way to calculate the p-value.
-#' @param scoring a character string specifying which scoring system to use.
+#' @param scoring a character string specifying the scoring system.
 #' @param alternative a character string specifying the alternative hypothesis.
 #' @param conf_level a number between zero and one indicating the confidence level to use.
 #' @param n_permu an integer indicating number of permutations for the permutation distribution. If set to `0`, all permutations will be used.
