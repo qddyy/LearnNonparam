d2ac38a8d7f68f5fae2e5379b242de936c164155
qddyy <isduyan@outlook.com>
2024-11-27 14:01:21 +0800

    remove missing values in `test()`

--- R/PermuTest.R
+++ R/PermuTest.R
@@ -443,7 +443,8 @@ PermuTest <- R6Class(
 )
 
 get_data <- function(call, env) {
-    data_exprs <- as.list(call)[-1]
+    data_exprs <- as.list.default(call)[-1]
+
     n_data <- length(data_exprs)
 
     if (n_data == 1 && is.list(data_1 <- eval(data_exprs[[1]], envir = env))) {
@@ -465,11 +466,19 @@ get_data <- function(call, env) {
             }
 
             data_i <- eval(data_exprs[[i]], envir = env)
+
             if (!is.numeric(data_i)) {
                 stop("Sample ", i, " is not numeric")
-            } else if (anyNA(data_i)) {
-                stop("Sample ", i, " contains NA")
-            } else data_i
+            }
+            if (anyNA(data_i)) {
+                warning("Sample ", i, " contains missing values, removed")
+                data_i <- data_i[!is.na(data_i)]
+            }
+            if (length(data_i) < 1) {
+                stop("Sample ", i, " does not contain enough observations")
+            }
+
+            data_i
         }
     ), data_names)
 }
