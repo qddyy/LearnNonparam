6017d1ad1f28b8731ba00ef413d4e7f576f592fc
qddyy <isduyan@outlook.com>
2024-12-15 14:22:14 +0800

    improve `permutation.hpp`

--- inst/include/pmt/permutation.hpp
+++ inst/include/pmt/permutation.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <iterator>
 
 template <typename T>
 T rand_int(T n)
@@ -9,41 +10,59 @@ T rand_int(T n)
 }
 
 template <typename T>
-void random_shuffle(T&& v)
+void random_shuffle(T first, T last)
 {
-    R_len_t n = v.size();
+    using diff_t = typename std::iterator_traits<T>::difference_type;
 
-    R_len_t j;
-    for (R_len_t i = 0; i < n - 1; i++) {
-        j = i + rand_int(n - i);
-        std::swap(v[i], v[j]);
+    diff_t n = std::distance(first, last);
+
+    for (diff_t i = 0; i < n - 1; i++) {
+        diff_t j = i + rand_int(n - i);
+        std::iter_swap(first + i, first + j);
     }
 }
 
 template <typename T>
-bool next_permutation(T&& v)
+bool next_permutation(T first, T last)
 {
-    return std::next_permutation(v.begin(), v.end());
+    return std::next_permutation(first, last);
 }
 
 template <typename T>
-double n_permutation(const T& v)
+double n_permutation(T first, T last)
 {
-    double A = 1;
+    double A = 1.0;
 
-    R_len_t n = v.size();
-    R_len_t n_i = 0;
+    typename std::iterator_traits<T>::difference_type rep = 0;
 
-    auto current = v[0];
-    for (R_len_t i = 0; i < n; i++) {
-        A *= (i + 1);
-        if (v[i] == current) {
-            A /= ++n_i;
+    auto val = *first;
+    for (auto it = first; it != last; it++) {
+        A *= std::distance(first, it) + 1;
+        if (*it == val) {
+            A /= ++rep;
         } else {
-            n_i = 1;
+            rep = 1;
+            val = *it;
         }
-        current = v[i];
     }
 
     return A;
+}
+
+template <typename T>
+auto random_shuffle(T v)
+{
+    return random_shuffle(v.begin(), v.end());
+}
+
+template <typename T>
+auto next_permutation(T v)
+{
+    return next_permutation(v.begin(), v.end());
+}
+
+template <typename T>
+auto n_permutation(T v)
+{
+    return n_permutation(v.begin(), v.end());
 }
