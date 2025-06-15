263a77cdd5679c0ad672e7b582ebf3de174d543d
qddyy <isduyan@outlook.com>
2025-06-16 00:25:41 +0800

    improve `multcomp_pmt`

--- src/pmt_interface.cpp
+++ src/pmt_interface.cpp
@@ -11,34 +11,16 @@ constexpr auto Rf_lang<2> = Rf_lang2;
 template <>
 constexpr auto Rf_lang<3> = Rf_lang3;
 
-template <bool sharing_args>
-class StatFunc : public Function {
+template <typename T>
+class CachedFunc : public Function {
 public:
     using Function::Function;
 
     template <typename... Args>
     auto operator()(Args&&... args) const
     {
-        return _invoke(Tag<sharing_args>(), std::forward<Args>(args)...);
-    }
-
-private:
-    template <bool>
-    struct Tag { };
-
-    template <typename... Args>
-    auto _invoke(Tag<false>, Args&&... args) const
-    {
-        return [R_closure = Function(Function::operator()(std::forward<Args>(args)...))](auto&&... args) {
-            return as<double>(R_closure(std::forward<decltype(args)>(args)...));
-        };
-    }
-
-    template <typename... Args>
-    auto _invoke(Tag<true>, Args&&... args) const
-    {
         return [R_call = Shield<SEXP>(Rf_lang<sizeof...(args) + 1>(Function::operator()(std::forward<Args>(args)...), std::forward<Args>(args)...))](auto&&...) {
-            return as<double>(Rcpp_fast_eval(R_call, R_GlobalEnv));
+            return as<T>(Rcpp_fast_eval(R_call, R_GlobalEnv));
         };
     }
 };
@@ -57,8 +39,8 @@ SEXP twosample_pmt(
     const bool progress)
 {
     return progress ?
-        impl_twosample_pmt<true, StatFunc<true>>(clone(x), clone(y), statistic_func, n_permu) :
-        impl_twosample_pmt<false, StatFunc<true>>(clone(x), clone(y), statistic_func, n_permu);
+        impl_twosample_pmt<true, CachedFunc<double>>(clone(x), clone(y), statistic_func, n_permu) :
+        impl_twosample_pmt<false, CachedFunc<double>>(clone(x), clone(y), statistic_func, n_permu);
 }
 
 #include "pmt/impl_ksample_pmt.hpp"
@@ -72,16 +54,35 @@ SEXP ksample_pmt(
     const bool progress)
 {
     return progress ?
-        impl_ksample_pmt<true, StatFunc<true>>(data, clone(group), statistic_func, n_permu) :
-        impl_ksample_pmt<false, StatFunc<true>>(data, clone(group), statistic_func, n_permu);
+        impl_ksample_pmt<true, CachedFunc<double>>(data, clone(group), statistic_func, n_permu) :
+        impl_ksample_pmt<false, CachedFunc<double>>(data, clone(group), statistic_func, n_permu);
 }
 
 #include "pmt/impl_multcomp_pmt.hpp"
 
+class PairwiseFunc : public Language {
+public:
+    PairwiseFunc(SEXP func) :
+        Language(func, IntegerVector(no_init(1)), IntegerVector(no_init(1)))
+    {
+        _ptr_i = INTEGER(CADR(*this));
+        _ptr_j = INTEGER(CADDR(*this));
+    }
+
+    double operator()(int i, int j) const
+    {
+        *_ptr_i = i;
+        *_ptr_j = j;
+        return as<double>(Language::fast_eval());
+    }
+
+private:
+    int* _ptr_i;
+    int* _ptr_j;
+};
+
 // [[Rcpp::export]]
 SEXP multcomp_pmt(
-    const SEXP group_i,
-    const SEXP group_j,
     const SEXP data,
     const SEXP group,
     const SEXP statistic_func,
@@ -89,8 +90,8 @@ SEXP multcomp_pmt(
     const bool progress)
 {
     return progress ?
-        impl_multcomp_pmt<true, StatFunc<false>>(group_i, group_j, data, clone(group), statistic_func, n_permu) :
-        impl_multcomp_pmt<false, StatFunc<false>>(group_i, group_j, data, clone(group), statistic_func, n_permu);
+        impl_multcomp_pmt<true, CachedFunc<PairwiseFunc>>(data, clone(group), statistic_func, n_permu) :
+        impl_multcomp_pmt<false, CachedFunc<PairwiseFunc>>(data, clone(group), statistic_func, n_permu);
 }
 
 #include "pmt/impl_paired_pmt.hpp"
@@ -104,8 +105,8 @@ SEXP paired_pmt(
     const bool progress)
 {
     return progress ?
-        impl_paired_pmt<true, StatFunc<true>>(clone(x), clone(y), statistic_func, n_permu) :
-        impl_paired_pmt<false, StatFunc<true>>(clone(x), clone(y), statistic_func, n_permu);
+        impl_paired_pmt<true, CachedFunc<double>>(clone(x), clone(y), statistic_func, n_permu) :
+        impl_paired_pmt<false, CachedFunc<double>>(clone(x), clone(y), statistic_func, n_permu);
 }
 
 #include "pmt/impl_rcbd_pmt.hpp"
@@ -118,8 +119,8 @@ SEXP rcbd_pmt(
     const bool progress)
 {
     return progress ?
-        impl_rcbd_pmt<true, StatFunc<true>>(clone(data), statistic_func, n_permu) :
-        impl_rcbd_pmt<false, StatFunc<true>>(clone(data), statistic_func, n_permu);
+        impl_rcbd_pmt<true, CachedFunc<double>>(clone(data), statistic_func, n_permu) :
+        impl_rcbd_pmt<false, CachedFunc<double>>(clone(data), statistic_func, n_permu);
 }
 
 #include "pmt/impl_association_pmt.hpp"
@@ -133,8 +134,8 @@ SEXP association_pmt(
     const bool progress)
 {
     return progress ?
-        impl_association_pmt<true, StatFunc<true>>(clone(x), clone(y), statistic_func, n_permu) :
-        impl_association_pmt<false, StatFunc<true>>(clone(x), clone(y), statistic_func, n_permu);
+        impl_association_pmt<true, CachedFunc<double>>(clone(x), clone(y), statistic_func, n_permu) :
+        impl_association_pmt<false, CachedFunc<double>>(clone(x), clone(y), statistic_func, n_permu);
 }
 
 #include "pmt/impl_table_pmt.hpp"
@@ -148,6 +149,6 @@ SEXP table_pmt(
     const bool progress)
 {
     return progress ?
-        impl_table_pmt<true, StatFunc<true>>(clone(row), clone(col), statistic_func, n_permu) :
-        impl_table_pmt<false, StatFunc<true>>(clone(row), clone(col), statistic_func, n_permu);
+        impl_table_pmt<true, CachedFunc<double>>(clone(row), clone(col), statistic_func, n_permu) :
+        impl_table_pmt<false, CachedFunc<double>>(clone(row), clone(col), statistic_func, n_permu);
 }
