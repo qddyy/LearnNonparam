f188858aabad59a932266313e75b0bac4188fd87
qddyy <isduyan@outlook.com>
2025-07-01 22:09:22 +0800

    add speed & ETA

--- inst/include/pmt/progress.hpp
+++ inst/include/pmt/progress.hpp
@@ -1,33 +1,39 @@
 #pragma once
 
 #include <array>
+#include <chrono>
 #include <utility>
 
 constexpr unsigned bar_width = 50;
 
-using progress_bar = std::array<char, bar_width + 19>;
+using progress_bar = std::array<char, bar_width + 46>;
 
 constexpr std::array<char, 10> num_char_map = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-template <unsigned n, unsigned... seq>
-constexpr progress_bar generate_bar(std::integer_sequence<unsigned, seq...>)
+template <unsigned percent, unsigned... bar_seq>
+constexpr progress_bar generate_bar(std::integer_sequence<unsigned, bar_seq...>)
 {
-    constexpr unsigned fill = n * bar_width / 100;
+    constexpr unsigned fill = percent * bar_width / 100;
     return {
         '\015',
+        '\033', '[', 'K',
         '\033', '[', '3', '1', 'm',
-        (n < 10) ? ' ' : num_char_map[n / 10],
-        (n < 10) ? num_char_map[n] : num_char_map[n % 10], '%',
+        (percent < 10) ? ' ' : num_char_map[percent / 10],
+        (percent < 10) ? num_char_map[percent] : num_char_map[percent % 10], '%', '%',
         '\033', '[', '3', '6', 'm',
-        ' ', '|', (seq + 1 < fill ? '-' : (seq + 1 == fill ? '>' : ' '))..., '|', ' ',
+        ' ', '|', (bar_seq + 1 < fill ? '-' : (bar_seq + 1 == fill ? '>' : ' '))..., '|', ' ',
+        '%', '.', '4', 'g', '/', 's',
+        '\033', '[', '3', '2', 'm',
+        ' ', 'E', 'T', 'A', ':', ' ',
+        '%', '.', '4', 'g', 's', ' ',
         '\0'
     };
 }
 
-template <unsigned... seq>
-constexpr std::array<progress_bar, sizeof...(seq)> generate_bars(std::integer_sequence<unsigned, seq...>)
+template <unsigned... percent_seq>
+constexpr std::array<progress_bar, sizeof...(percent_seq)> generate_bars(std::integer_sequence<unsigned, percent_seq...>)
 {
-    return { generate_bar<seq>(std::make_integer_sequence<unsigned, bar_width>())... };
+    return { generate_bar<percent_seq>(std::make_integer_sequence<unsigned, bar_width>())... };
 }
 
 constexpr auto generated_bars = generate_bars(std::make_integer_sequence<unsigned, 100>());
@@ -40,31 +46,26 @@ public:
         _progress_i(0) { }
 
     template <typename T>
-    void init(T&& update, R_xlen_t size)
+    void init(const T& update, R_xlen_t size)
     {
-        _init_buffer(size);
+        _init_statistic(update, size);
 
-        update();
-        _statistic = std::exchange(_buffer, NumericVector(0));
+        _buffer = NumericVector(0);
     }
 
     template <typename T>
-    void init(T&& update, R_xlen_t size, double n_permu)
+    void init(const T& update, R_xlen_t size, double n_permu)
     {
         double n = n_permu * size;
         if (n > R_XLEN_T_MAX) {
             stop("Too many permutations");
         }
 
-        _init_buffer(size);
-
-        update();
-        _statistic = _buffer;
+        _init_statistic(update, size);
 
         _init_buffer(static_cast<R_xlen_t>(n));
-
         if (size > 1) {
-            _buffer.attr("dim") = Dimension(size, n_permu);
+            _buffer.attr("dim") = IntegerVector::create(size, n_permu);
         }
 
         _init_progress();
@@ -81,38 +82,59 @@ public:
 
     explicit operator RObject()
     {
-        _clear_progress();
-
         _statistic.attr("permu") = _buffer;
 
         return _statistic;
-    };
+    }
+
+    ~Stat()
+    {
+        _clear_progress();
+    }
 
 private:
     RObject _statistic;
+
+    template <typename T>
+    void _init_statistic(const T& update, R_xlen_t size);
 
     NumericVector _buffer;
 
     R_xlen_t _buffer_size;
     R_xlen_t _buffer_i;
 
-    R_xlen_t _progress_every;
-    R_xlen_t _progress_i;
-
     void _init_buffer(R_xlen_t size)
     {
         _buffer = NumericVector(no_init(size));
 
-        _buffer_i = 0;
         _buffer_size = size;
+        _buffer_i = 0;
     }
+
+    R_xlen_t _progress_every;
+    R_xlen_t _progress_i;
 
     void _init_progress();
 
     void _update_progress();
 
     void _clear_progress();
+
+    double _speed;
+
+    std::chrono::steady_clock::time_point _time;
 };
+
+template <>
+template <typename T>
+void Stat<false>::_init_statistic(const T& update, R_xlen_t size)
+{
+    _init_buffer(size);
+
+    update();
+
+    _statistic = _buffer;
+}
 
 template <>
 void Stat<false>::_init_progress() { }
@@ -123,13 +145,37 @@ void Stat<false>::_update_progress() { }
 template <>
 void Stat<false>::_clear_progress() { }
 
+double elapsed(std::chrono::steady_clock::time_point& time)
+{
+    std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+    std::chrono::duration<double, typename std::chrono::seconds::period> elapsed = now - time;
+    time = now;
+
+    return elapsed.count();
+}
+
+template <>
+template <typename T>
+void Stat<true>::_init_statistic(const T& update, R_xlen_t size)
+{
+    _init_buffer(size);
+
+    _time = std::chrono::steady_clock::now();
+    update();
+    _speed = size / elapsed(_time);
+
+    _statistic = _buffer;
+}
+
 template <>
 void Stat<true>::_init_progress()
 {
-    _progress_i = 0;
     _progress_every = _buffer_size < 100 ? 1 : _buffer_size / 100;
+    _progress_i = 0;
 
-    Rcout << generated_bars[0].data();
+    double eta = _buffer_size / _speed;
+
+    Rprintf(generated_bars[0].data(), _speed, eta);
 }
 
 template <>
@@ -138,7 +184,11 @@ void Stat<true>::_update_progress()
     if (++_progress_i == _progress_every) {
         _progress_i = 0;
 
-        Rcout << generated_bars[static_cast<int>(100 * _buffer_i / _buffer_size)].data();
+        _speed = 0.2 * _speed + 0.8 * _progress_every / elapsed(_time);
+
+        double eta = (_buffer_size - _buffer_i) / _speed;
+
+        Rprintf(generated_bars[100 * _buffer_i / _buffer_size].data(), _speed, eta);
     }
 }
 
