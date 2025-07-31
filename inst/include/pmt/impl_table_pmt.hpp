65ca49070ca430a1e4beac0cbe3abfdc46501b63
qddyy <isduyan@outlook.com>
2025-07-31 11:17:24 +0800

    improve `distribution_pmt`

--- inst/include/pmt/impl_table_pmt.hpp
+++ inst/include/pmt/impl_table_pmt.hpp
@@ -1,33 +1,39 @@
 #pragma once
 
-template <bool progress, typename T>
-RObject impl_table_pmt(
-    IntegerMatrix data,
-    const T& statistic_func,
+template <typename T>
+struct table_traits;
+
+template <bool progress, typename T, typename U>
+RObject __impl_table_pmt(
+    T& data,
+    const U& statistic_func,
     const double n_permu)
 {
     Stat<progress> statistic_container;
 
+    using size_t = typename table_traits<T>::size_type;
+    using value_t = typename table_traits<T>::value_type;
+
     std::size_t n = 0;
-    for (R_xlen_t k = 0; k < data.size(); k++) {
-        n += data[k];
+    for (size_t k = 0; k < data.size(); k++) {
+        n += static_cast<std::size_t>(data[k]);
     }
 
-    std::vector<R_xlen_t> row;
-    std::vector<R_xlen_t> col;
+    std::vector<size_t> row;
+    std::vector<size_t> col;
     row.reserve(n);
     col.reserve(n);
-    for (R_xlen_t j = 0; j < data.ncol(); j++) {
-        for (R_xlen_t i = 0; i < data.nrow(); i++) {
-            for (int k = 0; k < data(i, j); k++) {
+    for (size_t j = 0; j < data.ncol(); j++) {
+        for (size_t i = 0; i < data.nrow(); i++) {
+            for (value_t k = 0; k < data(i, j); k++) {
                 row.emplace_back(i);
                 col.emplace_back(j);
             }
         }
     }
 
-    auto statistic_closure_ = [statistic_closure = statistic_func(data), &row, &col, data, n](auto&&... args) mutable {
-        for (R_xlen_t k = 0; k < data.size(); k++) {
+    auto statistic_closure_ = [statistic_closure = statistic_func(data), &data, &row, &col, n](auto&&... args) mutable {
+        for (size_t k = 0; k < data.size(); k++) {
             data[k] = 0;
         };
 
@@ -44,7 +50,7 @@ RObject impl_table_pmt(
     if (std::isnan(n_permu)) {
         statistic_container.init(table_update, 1);
     } else if (n_permu == 0) {
-        std::vector<R_xlen_t>& row_ = n_permutation(row) < n_permutation(col) ? row : col;
+        auto& row_ = n_permutation(row) < n_permutation(col) ? row : col;
 
         statistic_container.init(table_update, 1, n_permutation(row_));
 
@@ -63,6 +69,49 @@ RObject impl_table_pmt(
     return static_cast<RObject>(statistic_container);
 }
 
+template <>
+struct table_traits<IntegerMatrix> {
+    using size_type = R_xlen_t;
+    using value_type = int;
+};
+
+template <bool progress, typename T>
+RObject impl_table_pmt(
+    IntegerMatrix data,
+    const T& statistic_func,
+    const double n_permu)
+{
+    return __impl_table_pmt<progress>(data, statistic_func, n_permu);
+}
+
+class DistributionTable : public std::vector<double> {
+public:
+    void reserve(std::size_t n)
+    {
+        _ncol = n;
+
+        std::vector<double>::reserve(n * 2);
+    }
+
+    std::size_t nrow() const { return 2; }
+
+    std::size_t ncol() const { return _ncol; }
+
+    double& operator()(std::size_t i, std::size_t j)
+    {
+        return std::vector<double>::operator[](i + j * 2);
+    }
+
+private:
+    std::size_t _ncol;
+};
+
+template <>
+struct table_traits<DistributionTable> {
+    using size_type = std::size_t;
+    using value_type = double;
+};
+
 template <bool progress, typename T>
 RObject impl_distribution_pmt(
     const NumericVector x,
@@ -70,30 +119,41 @@ RObject impl_distribution_pmt(
     const T& statistic_func,
     const double n_permu)
 {
-    R_xlen_t m = x.size();
-    R_xlen_t n = y.size();
-
-    std::map<double, std::pair<R_xlen_t, R_xlen_t>> freq;
-    for (R_xlen_t i = 0; i < m; i++) {
+    std::map<double, std::pair<double, double>> freq;
+    for (R_xlen_t i = 0; i < x.size(); i++) {
         freq[x[i]].first++;
     }
-    for (R_xlen_t i = 0; i < n; i++) {
+    for (R_xlen_t i = 0; i < y.size(); i++) {
         freq[y[i]].second++;
     }
 
-    IntegerMatrix data(no_init(freq.size(), 2));
-    std::transform(freq.begin(), freq.end(), data.column(0).begin(), [](const auto& x) { return x.second.first; });
-    std::transform(freq.begin(), freq.end(), data.column(1).begin(), [](const auto& x) { return x.second.second; });
+    double n = freq.size();
+    if (n >= R_XLEN_T_MAX) {
+        stop("ECDF would be too long a vector");
+    }
 
-    NumericVector F(no_init(data.nrow() + 1));
-    NumericVector G(no_init(data.nrow() + 1));
+    DistributionTable data;
+    data.reserve(n++);
+    for (auto it = freq.begin(); it != freq.end(); it++) {
+        data.push_back(it->second.first);
+        data.push_back(it->second.second);
+    }
+
+    NumericVector F(no_init(n));
+    NumericVector G(no_init(n));
     *F.begin() = *G.begin() = 0.0;
 
-    auto build_ecdf = [m, n, F, G, data_F = data.column(0), data_G = data.column(1)]() mutable {
-        std::partial_sum(data_F.begin(), data_F.end(), F.begin() + 1);
-        std::partial_sum(data_G.begin(), data_G.end(), G.begin() + 1);
-        std::transform(F.begin() + 1, F.end(), F.begin() + 1, [m](double x) { return x / m; });
-        std::transform(G.begin() + 1, G.end(), G.begin() + 1, [n](double x) { return x / n; });
+    auto build_ecdf = [&data, F, G, n = static_cast<R_xlen_t>(n), inv_n_x = 1.0 / static_cast<double>(x.size()), inv_n_y = 1.0 / static_cast<double>(y.size())]() mutable {
+        double prob_F = 0.0;
+        double prob_G = 0.0;
+
+        std::size_t k = 0;
+        for (R_xlen_t i = 1; i < n; i++) {
+            prob_F += data[k++] * inv_n_x;
+            prob_G += data[k++] * inv_n_y;
+            F[i] = prob_F;
+            G[i] = prob_G;
+        }
     };
 
     auto distribution_decorator = [&build_ecdf, F, G](auto&& func) {
@@ -107,5 +167,5 @@ RObject impl_distribution_pmt(
         return distribution_decorator(statistic_func());
     };
 
-    return impl_table_pmt<progress>(data, statistic_func_, n_permu);
+    return __impl_table_pmt<progress>(data, statistic_func_, n_permu);
 }
